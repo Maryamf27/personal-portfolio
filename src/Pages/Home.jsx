4c439bbb7db2661dc84a3ec48601ba0f463ccf50
maryamf27 <maryam27cs@gmail.com>
2026-03-16 22:05:27 +0500

    updating portfolio tech stack

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -61,11 +61,10 @@ const CTAButton = memo(({ href, text, icon: Icon }) => (
             {text}
           </span>
           <Icon
-            className={`w-4 h-4 text-gray-200 ${
-              text === "Contact"
+            className={`w-4 h-4 text-gray-200 ${text === "Contact"
                 ? "group-hover:translate-x-1"
                 : "group-hover:rotate-45"
-            } transform transition-all duration-300 z-10`}
+              } transform transition-all duration-300 z-10`}
           />
         </span>
       </div>
@@ -157,11 +156,10 @@ const Home = () => {
       progressiveLoad: true,
     },
     style: { width: "100%", height: "100%" },
-    className: `w-full h-full transition-all duration-500 ${
-      isHovering
+    className: `w-full h-full transition-all duration-500 ${isHovering
         ? "scale-[180%] sm:scale-[160%] md:scale-[150%] lg:scale-[145%] rotate-2"
         : "scale-[175%] sm:scale-[155%] md:scale-[145%] lg:scale-[140%]"
-    }`,
+      }`,
   };
 
   return (
@@ -170,9 +168,8 @@ const Home = () => {
       id="Home"
     >
       <div
-        className={`relative z-10 transition-all duration-1000 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        }`}
+        className={`relative z-10 transition-all duration-1000 ${isLoaded ? "opacity-100" : "opacity-0"
+          }`}
       >
         <div className="container mx-auto min-h-screen">
           <div className="flex flex-col lg:flex-row items-center justify-center h-screen md:justify-between gap-0 sm:gap-12 lg:gap-20">
@@ -206,7 +203,7 @@ const Home = () => {
                 >
                   My name is Maryam Fatima, a Mern stack developer currently pursuing
                   my Bachelor in Computer Science from university
-                   of Education. I’m in my third year and deeply
+                  of Education. I’m in my third year and deeply
                   passionate about exploring new technologies. I enjoy building
                   clean, responsive, and user-friendly websites to solve
                   real-world problems.
@@ -260,28 +257,24 @@ const Home = () => {
             >
               <div className="relative w-full opacity-90">
                 <div
-                  className={`absolute inset-0 bg-gradient-to-r from-[#6366f1]/10 to-[#a855f7]/10 rounded-3xl blur-3xl transition-all duration-700 ease-in-out ${
-                    isHovering ? "opacity-50 scale-105" : "opacity-20 scale-100"
-                  }`}
+                  className={`absolute inset-0 bg-gradient-to-r from-[#6366f1]/10 to-[#a855f7]/10 rounded-3xl blur-3xl transition-all duration-700 ease-in-out ${isHovering ? "opacity-50 scale-105" : "opacity-20 scale-100"
+                    }`}
                 ></div>
 
                 <div
-                  className={`relative lg:left-12 z-10 w-full opacity-90 transform transition-transform duration-500 ${
-                    isHovering ? "scale-105" : "scale-100"
-                  }`}
+                  className={`relative lg:left-12 z-10 w-full opacity-90 transform transition-transform duration-500 ${isHovering ? "scale-105" : "scale-100"
+                    }`}
                 >
                   <DotLottieReact {...lottieOptions} />
                 </div>
 
                 <div
-                  className={`absolute inset-0 pointer-events-none transition-all duration-700 ${
-                    isHovering ? "opacity-50" : "opacity-20"
-                  }`}
+                  className={`absolute inset-0 pointer-events-none transition-all duration-700 ${isHovering ? "opacity-50" : "opacity-20"
+                    }`}
                 >
                   <div
-                    className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-gradient-to-br from-indigo-500/10 to-purple-500/10 blur-3xl animate-[pulse_6s_cubic-bezier(0.4,0,0.6,1)_infinite] transition-all duration-700 ${
-                      isHovering ? "scale-110" : "scale-100"
-                    }`}
+                    className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[400px] bg-gradient-to-br from-indigo-500/10 to-purple-500/10 blur-3xl animate-[pulse_6s_cubic-bezier(0.4,0,0.6,1)_infinite] transition-all duration-700 ${isHovering ? "scale-110" : "scale-100"
+                      }`}
                   ></div>
                 </div>
               </div>
